a7bc8e07742fce37809ffed8b6bf61361d3c0bb5
FerCesio <162721896+FerCesio@users.noreply.github.com>
2024-09-15 18:58:23 -0300

    Update App.java

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -1,14 +1,34 @@
 package com.template;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class App {
 
+    Map<String, Integer> accounts = new HashMap<>();
 
-    public static Map<String, Integer> deposit(Map<String, Integer> accounts, String account, int amount) {
-        return Collections.emptyMap();
+    public void createAccount(String name, int balance) {
+        accounts.put(name, balance);
     }
 
+    public void transfer(String initialAccount, String targetAccount, Integer amount) {
+        if (accounts.containsKey(initialAccount) && accounts.containsKey(targetAccount) && amount>0 && amount<=accounts.get(initialAccount)) {
+            accounts.put(targetAccount, accounts.get(targetAccount) + amount);
+            accounts.put(initialAccount, accounts.get(initialAccount) - amount);
+        }
+    }
+
+    public void deposit(String targetAccount, Integer amount) {
+        if (accounts.containsKey(targetAccount) && amount>0) {
+            accounts.put(targetAccount, accounts.get(targetAccount) + amount);
+        }
+    }
+
+    public void withdraw(String targetAccount, Integer amount) {
+        if (accounts.containsKey(targetAccount) && amount>0 && amount<=accounts.get(targetAccount)) {
+            accounts.put(targetAccount, accounts.get(targetAccount) - amount);
+        }
+    }
 
 }
